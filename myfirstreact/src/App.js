69ce33bf74d963a341d1a17f84249f869aeba09d
saltywalnut <67189141+saltywalnut@users.noreply.github.com>
2020-12-12 20:06:47 +0900

    js

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div >
-    <p>a</p>
+    <div>
+    <Name/>
+    <p>a k</p>
     </div>
   );
 }
+function popup() {
+  alert ("string")
+}
 
+function Name() {
+  return (
+    <div>
+    <h1> hello </h1>
+    <button onClick = {popup}>a</button>
+    </div>
+  )
+}
 export default App;
